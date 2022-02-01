c198eef1c8c8d92663d0bd2c66f5fa148abcd079
Aiden Pike <61250126+aidenpike@users.noreply.github.com>
2022-02-01 10:39:49 -0500

    Finish Moddability Branch
    2nd branch done. 3 to go.

--- The-cooler-flowchart-(C++).cpp
+++ The-cooler-flowchart-(C++).cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-int main(){
+int flowchart(){
        char choice = ' ';
-        int x, y = 0;
+        int x = 0;
         cout << "Welcome to the 3D Printer Finder.\n\n\n";
         do {
                 cout << "The thing I want most is...\n\n  [A] Ultra High Print Quality\n\n  [B] Moddability\n\n  [C] Ease of Use\n\n  [D] Big Build Volume\n\n  [E] Portability\n\n";  
@@ -33,8 +33,64 @@ int main(){
                                 cout << "You'll want the Ultimaker2.\n";
                         }        
                 }
-                else
-                        cout << "Invalid answer. Please try again.\n";
+                if (choice == 'B'){
+                        //Moddability
+                        choice = ' ';
+                        cout << "Do you also need affordability? [Y or N] ";
+                        cin >> choice;
+                        choice = toupper(choice);    
+                        if (choice == 'Y'){
+                                choice = ' ';
+                                cout << "DIY kit or assembled machine? [D or A] ";
+                                cin >> choice;
+                                choice = toupper(choice);    
+                                if (choice == 'D'){
+                                        choice = ' ';
+                                        cout << "Would you rather have more build volume or faster printing speeds? [V or S] ";
+                                        cin >> choice;
+                                        choice = toupper(choice);    
+                                        if (choice == 'V'){
+                                                choice = ' ';
+                                                cout << "A little bit more build volume or a LOT more build volume? [l or L (CASE SENSITIVE)]";
+                                                cin >> choice;
+                                                if (choice == 'l'){
+                                                        choice = ' ';
+                                                        cout << "Would you rather have better company support or better community support? [c or C (CASE SENSITIVE)] ";
+                                                        cin >> choice;
+                                                        if (choice == 'c')
+                                                                cout << "You'll want the Rigidbot Regular.\n";
+                                                        if (choice == 'C')
+                                                                cout << "You'll want the Printrbot Metal Plus.\n";
+                                                        
+                                                        
+                                                }                                                   
+                                                else if (choice == 'L')
+                                                        cout << "You'll want the Rigidbot Big.\n";
+                                        }
+                                }
+                                else if (choice == 'A')
+                                        cout << "You'll want the Robo3D.\n";
+                        }
+                        else if (choice == 'N'){
+                                choice = ' ';
+                                cout << "DIY kit or assembled machine? [D or A] ";
+                                cin >> choice;
+                                choice = toupper(choice);    
+                                if (choice == 'D')
+                                        cout << "You'll want the Luizbot KITTAZ.\n";
+                                else if (choice == 'A')
+                                        cout << "You'll want the Luizbot TAZ 4.\n";
+                        }
+                }
+                if (choice == 'C'){
+                        
+                }
+                if (choice == 'D'){
+                        
+                }
+                if (choice == 'E'){
+                        
+                }
         } while (x != 1); 
 }
 
