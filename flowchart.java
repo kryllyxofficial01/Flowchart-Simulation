3fa9071741bbf01850c40c6e841a982e5b06295d
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-02-04 16:14:10 +0000

    finished second branch

--- flowchart.java
+++ flowchart.java
@@ -33,15 +33,94 @@ public class flowchart {
 
     printQuality.close();
   }
+
+  public static void moddabilityBranch() {
+    Scanner moddability = new Scanner(System.in);
+
+    System.out.println("");
+    System.out.println("Do you want affordability? [Y]es or [N]o?:");
+    String affordability = moddability.next();
+
+    if (affordability.equals("Y")) {
+      System.out.println("");
+      System.out.println("What kit do you want? [D]IY or [P]re-assembled?:");
+      String kit_type = moddability.next();
+
+      if (kit_type.equals("D")) {
+        System.out.println("");
+        System.out.println("Do you want [M]ore Build Volume or [F]aster Printing Speed?:");
+        String capability = moddability.next();
+
+        if (capability.equals("M")) {
+          System.out.println("");
+          System.out.println("How much more? A [B]it More or a [L]ot More?:");
+          String build_amount = moddability.next();
+
+          if (build_amount.equals("B")) {
+            System.out.println("");
+            System.out.println("Would you rather have better [C]ompany Support or better C[O]mmunity Support?:");
+            String support_type = moddability.next();
+
+            if (support_type.equals("C")) {
+              System.out.println("");
+              System.out.println("");
+              System.out.println("Recommendation: Printrbot Metal Plus");
+            }
+            else if (support_type.equals("O")) {
+              System.out.println("");
+              System.out.println("");
+              System.out.println("Recommendation: Rigidbot Regular");
+            }
+          }
+          else if (build_amount.equals("L")) {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Recommendation: Rigidbot Big");
+          }
+        }
+        else if (capability.equals("F")) {
+          System.out.println("");
+          System.out.println("");
+          System.out.println("Recommendation: Velleman K8200");
+        }
+      }
+      else if (kit_type.equals("P")) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Recommendation: Robo3D");
+      }
+    }
+    else if (affordability.equals("N")) {
+      System.out.println("");
+      System.out.println("What kit do you want? [D]IY or [P]re-assembled?:");
+      String kit_type = moddability.next();
+
+      if (kit_type.equals("D")) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Recommendation: Lulzbot KITTAZ");
+      }
+      else if (kit_type.equals("P")) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Recommendation: Lulzbot TAZ 4");
+      }
+    }
+
+    moddability.close();
+  }
   
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("What do you want most from your 3D Designer? [U]ltra High Print Quality, [M]odability, [E]ase of Use, [B]ig Build Volume, or [P]ortability?:");
+    System.out.println("What do you want most from your 3D Designer? [U]ltra High Print Quality, [M]oddability, [E]ase of Use, [B]ig Build Volume, or [P]ortability?:");
     String purpose = scanner.next();
 
     if (purpose.equals("U")) {
       printQualityBranch();
+    }
+    else if (purpose.equals("M")) {
+      moddabilityBranch();
     }
     
     scanner.close();
