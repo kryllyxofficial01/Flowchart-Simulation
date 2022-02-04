92a0682faa684cc92b5b28475eaf808f6f2ea580
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-02-04 14:33:51 +0000

    initialized print quality branch method

--- flowchart.java
+++ flowchart.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class flowchart {
+
+  public static void printQualityBranch() {
+    Scanner printQuality = new Scanner(System.in);
+  }
+  
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    String affordability = "";
-    String kit_type = "";
 
     System.out.println("What do you want most from your 3D Designer? [U]ltra High Print Quality, [M]odability, [E]ase of Use, [B]ig Build Volume, or [P]ortability?:");
     String purpose = scanner.next();
